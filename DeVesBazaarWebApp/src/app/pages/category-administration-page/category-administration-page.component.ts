3ee1ac0ed27112ab51b95c3c34df324b1a3614c5
Sven Reichert <Sven.Reichert@generic.de>
2021-02-03 22:15:21 +0100

    Category Page title bar plus action Buttons

--- DeVesBazaarWebApp/src/app/pages/category-administration-page/category-administration-page.component.ts
+++ DeVesBazaarWebApp/src/app/pages/category-administration-page/category-administration-page.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryAdministrationPageComponent implements OnInit {
 
+
   constructor() { }
+
 
   ngOnInit(): void {
   }
 
+
+  onAddNewCategory(): void {
+
+  }
+
+  onReLoadCategory(): void {
+
+  }
+  
 }
